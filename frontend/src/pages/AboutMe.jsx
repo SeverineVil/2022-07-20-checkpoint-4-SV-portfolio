e9f7ba828f43983eb1ede9bfc37b89fbaae3a1c7
Séverine <92460484+SeverineVil@users.noreply.github.com>
2022-07-21 22:38:18 +0200

    css card project

---
+++ frontend/src/pages/AboutMe.jsx
@@ -0,0 +1,11 @@
+import "./styles/AboutMe.css";
+
+import NeonLight from "../components/NeonLight";
+
+export default function AboutMe() {
+  return (
+    <div className="home-container">
+      <NeonLight />
+    </div>
+  );
+}
--- frontend/src/pages/Body.jsx
+++ frontend/src/pages/Body.jsx
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Login from "./Login";
 import ProjectsList from "./ProjectsList";
 import Header from "../components/Header";
+import AboutMe from "./AboutMe";
 
 function Body() {
   return (
@@ -10,6 +11,7 @@ function Body() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
         <Route path="/projects" element={<ProjectsList />} />
         <Route path="/admin" element={<Login />} />
       </Routes>
