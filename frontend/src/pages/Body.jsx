39ce4c19285eb9545af99abc8d2b6d2c813af2e9
Séverine <92460484+SeverineVil@users.noreply.github.com>
2022-07-22 09:30:02 +0200

    last commit

--- frontend/src/pages/Body.jsx
+++ frontend/src/pages/Body.jsx
@@ -1,19 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
+import Admin from "./Admin";
 import ProjectsList from "./ProjectsList";
-import Header from "../components/Header";
+import Contact from "./Contact";
+
 import AboutMe from "./AboutMe";
 
 function Body() {
   return (
     <div>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutme" element={<AboutMe />} />
         <Route path="/projects" element={<ProjectsList />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/admin" element={<Login />} />
+        <Route path="/login/myadmin" element={<Admin />} />
       </Routes>
     </div>
   );
